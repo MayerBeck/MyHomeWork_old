51972d0a98cb9177d03ec8ac5d16e3446b81a123
MayerBeck <mayerbeck83@gmail.com>
2022-10-17 00:36:54 +0300

    finish commit hw2

--- HM_2/Program.cs
+++ HM_2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+
+*/
+
+Console.Write("Input fisrt number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max;
+
+if(number1 > number2)
+    {
+    if(number1 > number3) max = number1;
+    else max = number2;
+    }
+else
+{
+if(number2 > number3) max = number2;
+else max = number3;
+}
+Console.WriteLine($"Большое число из {number1}, {number2} и {number3} это {max}");
