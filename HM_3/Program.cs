9f3940b143c9a5a2894e99ca3dafc18b9451cdd0
MayerBeck <mayerbeck83@gmail.com>
2022-10-17 09:51:09 +0300

    finish commit hw3

--- HM_3/Program.cs
+++ HM_3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine($"{number} Четное число ");
+}
+else
+{
+    Console.WriteLine($"{number} Не четное число ");
+}
