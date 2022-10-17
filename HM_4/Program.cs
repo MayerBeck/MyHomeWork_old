7ff5ae7ca406bc7a8fd0af812307c5c0bb197ce8
MayerBeck <mayerbeck83@gmail.com>
2022-10-17 10:55:50 +0300

    finish commit hw4

--- HM_4/Program.cs
+++ HM_4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+  for(int i=1; i<=number; i++)
+   if(i%2 == 0) 
+{
+    Console.Write($"{i} ");
+}
