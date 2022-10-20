1c837cc7e7b80f3752e97b1a73d1110143048c7d
MayerBeck <mayerbeck83@gmail.com>
2022-10-20 20:01:55 +0300

    finish commit hm_2 sem_2

---
+++ HM_6/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Enter any number: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine($"The third digit is -> {anyNumberText[2]}");
+}
+else 
+{
+  Console.WriteLine("There is no third digit");
+}
