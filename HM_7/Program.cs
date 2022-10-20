4f72fdcab4bcc305b482638007586bd0eeb4ae18
MayerBeck <mayerbeck83@gmail.com>
2022-10-20 20:18:38 +0300

    finish commit hm_2 sem_2

---
+++ HM_7/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("Этот день выходной -> да");
+  }
+  else Console.WriteLine("этот день не выходной -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
