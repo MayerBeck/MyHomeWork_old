f9b36921fad307dd1ed7262318b78990fc2108b2
MayerBeck <mayerbeck83@gmail.com>
2022-11-13 10:32:13 +0300

    finish commit hm_2 sem_4

---
+++ HM_12/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.*/
+
+int sum = 0;
+
+while(true)
+{
+    Console.WriteLine("Введите целое число:");
+
+    if(int.TryParse(Console.ReadLine(), out int number))
+    {
+        Console.WriteLine("Сумма всех цифр числа " + number + " равна " + Calculate(number));
+        break;
+    }
+    else Console.WriteLine("Некорректно указано число!\n");
+}
+
+int Calculate(int number)
+{
+    string str = Math.Abs(number).ToString();
+    for(int i = 0; i < str.Length; i++)
+    {
+        int.TryParse(str[i].ToString(), out int num);
+        sum += num;
+    }
+    return sum;
+}
