0c9c6c5b3180a38229ffe08d8f35c3ef310ef594
MayerBeck <mayerbeck83@gmail.com>
2022-11-16 09:32:01 +0300

    finish commit hm_2 sem_5

---
+++ HM_15/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.*/
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+} 
+
+int SumOddElements(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    return sum;    
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+Console.WriteLine("Input a numbers of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a min possible value:"); 
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a max possible value:");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
+Console.WriteLine(SumOddElements(myArray));
