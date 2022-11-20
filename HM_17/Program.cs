d5a82c55c939a7db801c01449fe9b81762e874d6
MayerBeck <mayerbeck83@gmail.com>
2022-11-20 22:45:27 +0300

    finish commit hm_1 sem 6

---
+++ HM_17/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+int PositiveDigit(int m)
+{
+    int count =0;
+    int num;
+    for (int dig=0; dig <m; dig++)
+    {
+        Console.Write("Введите число: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if(num > 0)
+        count++;
+    }
+    return count;
+}
+Console.Write("Сколько чисел: ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(PositiveDigit(m));
