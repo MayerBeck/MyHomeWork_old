f002caaa4eb3194455de5ece7179c7fa5ad85b40
MayerBeck <mayerbeck83@gmail.com>
2022-11-20 23:04:42 +0300

    finish commit hm_2 sem_6

---
+++ HM_18/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.*/
+
+Console.WriteLine();
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+
+LinearEqations(k1, k2, b1, b2);
+
+void LinearEqations(double m, double n, double a, double b)
+{
+double x = 0;
+double y = 0;    
+    if (m == n) Console.WriteLine("Уравнение не имеет решений!");
+    else
+    {
+        x = (b - a) / (m - n);
+        y = m*x + a;
+    }
+    Console.WriteLine($"Решение уравнения (x, y): ({x}, {y})");
+    Console.WriteLine();
+}
