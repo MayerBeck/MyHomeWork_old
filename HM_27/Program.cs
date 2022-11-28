38db51496c53a6b42698829b20a4cd7121a5a170
MayerBeck <mayerbeck83@gmail.com>
2022-11-28 14:04:54 +0300

    finish commit hm_1 sem_9

---
+++ HM_27/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии. */
+
+
+int RecursMetod(int N)
+{
+    if (N == 0) return 0;
+    Console.Write(N + " ");
+    return RecursMetod(N - 1);
+}
+Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine()!);
+
+RecursMetod(N);
